9a0d07bd213c0cd338c1174ddeb7abd64f363589
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-17 19:56:27 -0400

    imported the navbar from components and rendered it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import logo from './logo.svg';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h2>Your Portfolio</h2>;
+  return <Navbar />;
 }
 
 export default App;
