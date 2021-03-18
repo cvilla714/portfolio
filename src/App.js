5ad1a309ec8b4e4d5e0452c68c4221f3246d26cb
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-18 02:33:12 -0400

    imported the portfolio component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import Porftolio from "./components/Porftolio";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Porftolio />
     </>
   );
 }
