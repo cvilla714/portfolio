36cdfe914adc35e8dbe316ac6f4d7b390069fdb4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-18 12:26:14 -0400

    added FontAwesome icon and overflow divs

--- src/components/Porftolio.js
+++ src/components/Porftolio.js
@@ -5,6 +5,9 @@ import restaurant from "../images/restaurant.png";
 import todolist from "../images/todolist.png";
 import weather from "../images/weatherapp.png";
 import article from "../images/lifestylearticle.png";
+// Font Awesome Imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Porftolio = () => {
   return (
@@ -14,31 +17,43 @@ const Porftolio = () => {
         <div className="image-box-wrapper row justify-content-center">
           <div className="portfolio-image-box">
             <img className="portfolio-image" src={linter} alt="linter project" />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/*-*/}
 
           <div className="portfolio-image-box">
             <img className="portfolio-image" src={game} alt="Phaser Game Endless Runner" />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/*-*/}
 
           <div className="portfolio-image-box">
             <img className="portfolio-image" src={restaurant} alt="Restaurante Page Project" />
+            <div className="overflow"></div>
+            * <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/*-*/}
 
           <div className="portfolio-image-box">
             <img className="portfolio-image" src={todolist} alt="Todo List Project" />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/*-*/}
 
           <div className="portfolio-image-box">
             <img className="portfolio-image" src={weather} alt="Build A Weather App" />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
           {/*-*/}
 
           <div className="portfolio-image-box">
             <img className="portfolio-image" src={article} alt="Rails Capstone" />
+            <div className="overflow"></div>
+            <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
           </div>
         </div>
       </div>
