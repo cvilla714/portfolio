560536003e7a661b6042be88f9915e2cd787386f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-18 00:33:23 -0400

    added the paticles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 // import logo from './logo.svg';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "square",
+              stroke: {
+                width: 9,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
