c8c0f9d8cedaee73ce40dad58ad407d8b4f7fa47
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-18 01:19:59 -0400

    imported aboutme

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
       />
       <Navbar />
       <Header />
+      <AboutMe />
     </>
   );
 }
