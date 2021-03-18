3d724ed4581f9dcc71bbdc101f7079736461c9dc
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-18 00:32:22 -0400

    added the header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Web development and websites promotions</h1>
+        <Typed className="typed-text" strings={["HTML/CSS", "Ruby", "Ruby on Rails", "Javascript", "Webpack", "Phaser", "NOde.js", "ExpressJs", "MongoDB", "Moongoose", "PostGreSql", "SQL", "React"]} typeSpeed={40} backSpeed={60} loop />
+        <a href="#" className="btn-main-offer">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
