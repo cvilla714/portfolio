004f1bcb59372b90e25dfb33614a51ec2baa8325
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-19 02:30:59 -0400

    eliminated unused tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,6 @@ const Navbar = () => {
           <img className="logo" src={logo} alt="logo..." />
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          {/* <span className="navbar-toggler-icon"></span> */}
           <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
         </button>
 
@@ -26,16 +25,6 @@ const Navbar = () => {
             <li className="nav-item">
               <a className="nav-link" href="#">
                 about me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                how work
               </a>
             </li>
             <li className="nav-item">
