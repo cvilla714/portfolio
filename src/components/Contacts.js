c471308f7398679136849a757f43444731ed83ae
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-19 14:10:25 -0400

    added the correspoding id

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -37,7 +37,7 @@ const Contact = () => {
   };
 
   return (
-    <div className="contacts">
+    <div id="contact" className="contacts">
       <div className="text-center">
         <h1>contact me</h1>
         <p>Please Fill out the form and describe your project needs and I'll contact you as soon as possible.</p>
