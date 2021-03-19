5cd4b1c3fba2f4510750c31916e76be46d6731d1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-19 14:10:45 -0400

    added my resume and option to download it

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,9 @@
 import React from "react";
 import author from "../me.jpg";
-
+import resume from "../images/cosmelresume.pdf";
 const AboutMe = () => {
   return (
-    <div className="container py-5">
+    <div id="about" className="container py-5">
       <div className="row">
         <div className="col-lg-6 col-xm-12">
           <div className="photo-wrap mb-5">
@@ -13,9 +13,14 @@ const AboutMe = () => {
         <div className="col-lg-6 col-xm-12">
           <h1 className="about-heading">about me</h1>
           <p>
-            Hello I am Cosmel, I'am a Full-Stack Web Developer. Technologies I use include HTML/CSS, Bootstrap, Sass, PostgreSQL,SQL,MongoDB, MoonGose,NodeJS, ExpressJS, ServerJS, JavaScript,Webpack, Phaser, ReactJs I create responsive websites that are displayed on all devices desktops and
-            smartphones
+            Hello, My name is Cosmel, I'm a Full-Stack Web Developer. These are some of the Technologies that I have used HTML/CSS, Bootstrap, Sass, PostgreSQL, SQL, MongoDB, MoonGose, NodeJS, ExpressJS, ServerJS, JavaScript, Webpack, Phaser, ReactJs.
+            <br /> I create responsive websites that are displayed on all devices, desktops, and smartphones.
           </p>
+
+          <a href={resume} download="cosmelresume.pdf">
+            {" "}
+            <button className="btn btn-info"> Get my Resume</button>
+          </a>
         </div>
       </div>
     </div>
