8e6fc5541afa07c69effefecee8ac5dc9bba52ba
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-19 15:01:24 -0400

    added the home id

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Typed from "react-typed";
 
 const Header = () => {
   return (
-    <div className="header-wrapper">
+    <div id="home" className="header-wrapper">
       <div className="main-info">
         <h1 className="main-header">Full-Stack Developer</h1>
         <Typed className="typed-text" strings={["HTML/CSS", "Ruby", "Ruby on Rails", "JavaScript", "Webpack", "Phaser", "NodeJS", "ExpressJs", "MongoDB", "Mongoose", "Oracle", "PostGreSql", "SQL", "React"]} typeSpeed={40} backSpeed={60} loop />
