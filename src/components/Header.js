1b7064932c05b547dd77d5682dd4106885db8fa1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-19 02:31:23 -0400

    included more content to the typed text

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="header-wrapper">
       <div className="main-info">
         <h1 className="main-header">Full-Stack Developer</h1>
-        <Typed className="typed-text" strings={["HTML/CSS", "Ruby", "Ruby on Rails", "Javascript", "Webpack", "Phaser", "NOde.js", "ExpressJs", "MongoDB", "Moongoose", "PostGreSql", "SQL", "React"]} typeSpeed={40} backSpeed={60} loop />
+        <Typed className="typed-text" strings={["HTML/CSS", "Ruby", "Ruby on Rails", "JavaScript", "Webpack", "Phaser", "NodeJS", "ExpressJs", "MongoDB", "Mongoose", "Oracle", "PostGreSql", "SQL", "React"]} typeSpeed={40} backSpeed={60} loop />
         <a href="#" className="btn-main-offer">
           Contact Me
         </a>
