608c554c705657076026eedd62a32f444a87c579
Cosmel Villalobos <cosmelvg@gmail.com>
2022-02-06 20:08:02 -0500

    New content
    update content, added Redux and RTK Query

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,6 @@
-import React from "react";
-import author from "../me.jpg";
-import resume from "../images/cosmelresume.pdf";
+import React from 'react';
+import author from '../me.jpg';
+import resume from '../images/cosmelresume.pdf';
 const AboutMe = () => {
   return (
     <div id="about" className="container py-5">
@@ -13,12 +13,16 @@ const AboutMe = () => {
         <div className="col-lg-6 col-xm-12">
           <h1 className="about-heading">about me</h1>
           <p>
-            Hello, My name is Cosmel, I'm a Full-Stack Web Developer. These are some of the Technologies that I have used HTML/CSS, Bootstrap, Sass, PostgreSQL, SQL, MongoDB, MoonGose, NodeJS, ExpressJS, ServerJS, JavaScript, Webpack, Phaser, ReactJs.
-            <br /> I create responsive websites that are displayed on all devices, desktops, and smartphones.
+            Hello, My name is Cosmel, I'm a Full-Stack Web Developer. These are
+            some of the Technologies that I have used HTML/CSS, Bootstrap, Sass,
+            PostgreSQL, SQL, MongoDB, MoonGose, NodeJS, ExpressJS, ServerJS,
+            JavaScript, Webpack, Phaser, ReactJs,Redux and RTK Query.
+            <br /> I create responsive websites that are displayed on all
+            devices, desktops, and smartphones.
           </p>
 
           <a href={resume} download="cosmelresume.pdf">
-            {" "}
+            {' '}
             <button className="btn btn-info"> Get my Resume</button>
           </a>
         </div>
