8282516d363c5f170757e0c26e997c6102735f0c
Cosmel Villalobos <cosmelvg@gmail.com>
2022-02-06 20:09:36 -0500

    Add new skills
    add new skills Redux and RTQ Query

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
-import React from "react";
-import Typed from "react-typed";
+import React from 'react';
+import Typed from 'react-typed';
 
 const Header = () => {
   return (
     <div id="home" className="header-wrapper">
       <div className="main-info">
         <h1 className="main-header">Full-Stack Developer</h1>
-        <Typed className="typed-text" strings={["HTML/CSS", "Ruby", "Ruby on Rails", "JavaScript", "Webpack", "Phaser", "NodeJS", "ExpressJs", "MongoDB", "Mongoose", "Oracle", "PostGreSql", "SQL", "React"]} typeSpeed={40} backSpeed={60} loop />
+        <Typed
+          className="typed-text"
+          strings={[
+            'HTML/CSS',
+            'Ruby',
+            'Ruby on Rails',
+            'JavaScript',
+            'Webpack',
+            'Phaser',
+            'NodeJS',
+            'ExpressJs',
+            'MongoDB',
+            'Mongoose',
+            'Oracle',
+            'PostGreSql',
+            'SQL',
+            'React',
+            'Redux',
+            'RTK Query',
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
         <a href="#" className="btn-main-offer">
           Contact Me
         </a>
