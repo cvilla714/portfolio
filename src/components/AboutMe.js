17684db9d7534839a7c9b9cb389d5316b80ab886
Cosmel Villalobos <cosmelvg@gmail.com>
2024-06-02 16:03:01 -0400

    Update AboutMe component to include categorized technologies and adjust profile image size

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import author from '../me.jpg';
 import resume from '../images/cosmelresume.pdf';
+
 const AboutMe = () => {
   return (
     <div id="about" className="container py-5">
@@ -11,19 +12,19 @@ const AboutMe = () => {
           </div>
         </div>
         <div className="col-lg-6 col-xm-12">
-          <h1 className="about-heading">about me</h1>
-          <p>
-            Hello, My name is Cosmel, I'm a Full-Stack Web Developer. These are
-            some of the Technologies that I have used HTML/CSS, Bootstrap, Sass,
-            PostgreSQL, SQL, MongoDB, MoonGose, NodeJS, ExpressJS, ServerJS,
-            JavaScript, Webpack, Phaser, ReactJs,Redux and RTK Query.
-            <br /> I create responsive websites that are displayed on all
-            devices, desktops, and smartphones.
-          </p>
+          <h1 className="about-heading">About Me</h1>
+          <p>Hello, My name is Cosmel, I'm a Full-Stack Web Developer. Here are some of the technologies I have used:</p>
+          <h2>Front-End Technologies</h2>
+          <p>HTML/CSS, Bootstrap, Sass, JavaScript, ReactJS, Redux, RTK Query, Webpack, Phaser</p>
+
+          <h2>Back-End Technologies</h2>
+          <p>NodeJS, ExpressJS, ServerJS, PostgreSQL, SQL, MongoDB, Mongoose, Ruby, Ruby on Rails, Python, Flask</p>
+
+          <h2>DevOps Tools</h2>
+          <p>Git, Docker, Kubernetes, AWS</p>
 
           <a href={resume} download="cosmelresume.pdf">
-            {' '}
-            <button className="btn btn-info"> Get my Resume</button>
+            <button className="btn btn-info">Get my Resume</button>
           </a>
         </div>
       </div>
